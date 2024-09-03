0082bf58e9b980f67f02d9d437d21be43df3b21c
ppalmero <pablorafaelpalmero@gmail.com>
2024-09-03 14:19:30 -0300

    encabezado

--- android/app/src/main/java/com/promocion/unsl/MainActivity.java
+++ android/app/src/main/java/com/promocion/unsl/MainActivity.java
@@ -2,4 +2,10 @@ package com.promocion.unsl;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+
+
+
+
+}
